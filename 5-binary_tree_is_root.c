3fbec513ecdcd542f5a11c4d5a63a76653a6f490
Diana Henao <2777@holbertonschool.com>
2021-07-23 00:19:01 +0000

    :hammer: Add development script 5-binary_tree_is_root.c and update header file binary_trees.h

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_root - C - Binary trees
+* @node: A pointer to the node to check.
+*
+* Description: Function that checks if a given node is
+* a root
+* Return: 1 if node is a root, otherwise 0.
+*/
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+
+	if (node->parent != NULL)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (1);
+	}
+}
